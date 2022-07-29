ab281bca02466b5d506999b2a76abedf76b7dd14
Ros <46716815+RosBoyes@users.noreply.github.com>
2022-07-29 14:59:16 +0100

    Start screen + which order?

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,13 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// router.post('/which-part', function (req, res) {
+//   req.session.data['working'] = 'yes';
+//     res.redirect('/vary-an-order/2-which-part')
+// })
+
+router.post('/which-part-answer', function (req, res) {
+  res.redirect('/vary-an-order/2-which-part')
+})
+
 module.exports = router
